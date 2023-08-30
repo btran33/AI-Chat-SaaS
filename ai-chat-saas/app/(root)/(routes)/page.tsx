caf5040236d81edce81ea01810eee8bf17515a11
Ben Tran <btran33@illinois.edu>
2023-08-30 14:30:34 -0500

    Add logic for retreiving buddies based on search params, add front-end cards for displaying buddies and empty image when no search is found

--- ai-chat-saas/app/(root)/(routes)/page.tsx
+++ ai-chat-saas/app/(root)/(routes)/page.tsx
@@ -1,12 +1,52 @@
 import prismaDB from "@/lib/prismadb";
 import { Categories } from "@/components/categories";
+import { Buddies } from "@/components/buddies";
+import { Buddy } from "@prisma/client";
 
-const RootPage = async () => {
+interface RootPageProps {
+    searchParams: {
+        categoryId: string;
+        name: string;
+    }
+}
+
+const RootPage = async ({
+    searchParams
+}: RootPageProps) => {
+
+    // let data : (Buddy & {
+    //     _count: {
+    //         messages: number
+    //     }
+    // })[] = []
+
+    // get all buddies from url params, ordered by creation date
+    // and message counts of each buddy 
+    const data = await prismaDB.buddy.findMany({
+        where: {
+            categoryId: searchParams.categoryId,
+            name: {
+                search: searchParams.name
+            }
+        },
+        orderBy: {
+            createdAt: "desc"
+        },
+        include: {
+            _count: {
+                select: {
+                    messages: true
+                }
+            }
+        }
+    })
+
     const category = await prismaDB.category.findMany()
     
     return ( 
         <div className="h-full p-4 space-y-2">
             <Categories data={category} />
+            <Buddies data={data} />
         </div>
      );
 }
