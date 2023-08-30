6c17d4ce9c868f43e2dab346d7263a1626be6bac
Ben Tran <btran33@illinois.edu>
2023-08-30 16:48:06 -0500

    Add AI tooling from vercel to project

--- ai-chat-saas/app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ ai-chat-saas/app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -2,6 +2,9 @@
 
 import { ChatHeader } from "@/components/chat-header"
 import { Buddy, Message } from "@prisma/client"
+import { useRouter } from "next/navigation"
+import { FormEvent, useState } from "react"
+import { useCompletion } from "ai/react"
 
 interface ChatClientProps {
     buddy: Buddy & {
@@ -15,6 +18,38 @@ interface ChatClientProps {
 export const ChatClient = ({
     buddy
 }: ChatClientProps) => {
+    const router = useRouter()
+    const [messages, setMessages] = useState<any[]>(buddy.messages)
+
+    const {
+        input,
+        isLoading,
+        handleInputChange,
+        handleSubmit,
+        setInput
+    } = useCompletion({
+        api: `/api/chat/${buddy.id}`,
+        onFinish(_, completion) {
+            const systemMessage = {
+                role: 'system',
+                content: completion
+            };
+
+            setMessages((current) => [...current, systemMessage])
+            setInput("")
+            router.refresh()
+        }
+    })
+
+    const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+        const userMessage = {
+            role: 'user',
+            content: input
+        }
+
+        setMessages((current) => [...current, userMessage])
+        handleSubmit(event)
+    }
 
     return (
         <div className="flex flex-col h-full p-4 space-y-2">
