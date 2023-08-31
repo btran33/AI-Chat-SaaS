2004c66c3cd38da9328e387c8ffdd1220a82e23d
Ben Tran <btran33@illinois.edu>
2023-08-31 15:45:30 -0500

    Add to Memory Manager: an instance getter; Redis buddy key generator; write/read to Redis chat history, and seeding the chat history to Redis

--- ai-chat-saas/lib/memory-services.ts
+++ ai-chat-saas/lib/memory-services.ts
@@ -2,6 +2,7 @@ import { Redis } from '@upstash/redis'
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
 import { PineconeStore } from 'langchain/vectorstores/pinecone'
 import { PineconeClient } from '@pinecone-database/pinecone'
+import { type } from 'os'
 
 export type BuddyKey = {
     buddyName: string;
@@ -72,7 +73,7 @@ export class MemoryManager {
     }
 
     /**
-     * A static function to return an instance of Memory Manager,
+     * A public function to return an instance of Memory Manager,
      * initializing one if there is not an existing instance already
      * @returns a Memory Manager instance
      */
@@ -80,10 +81,95 @@ export class MemoryManager {
         if (!MemoryManager.instance) {
             MemoryManager.instance = new MemoryManager();
             await MemoryManager.instance.init()
-        }
+        };
 
         return MemoryManager.instance;
     }
 
-    
+    /**
+     * A private fucntion to generate a Redis key for a Buddy
+     * @param buddyKey a Prop containing the name, model, and userId of the AI Buddy
+     * @returns a string representing the Redis key for the Buddy
+     */
+    private generateRedisBuddyKey(buddyKey: BuddyKey): string {
+        return `${buddyKey.buddyName}-${buddyKey.modelName}-${buddyKey.userId}`;
+    }
+
+    /**
+     * A public function to write the text input (user or system) to
+     * Redis chat history
+     * @param input the text input, in string
+     * @param buddyKey the Prop containing the name, model, and userId of the AI Buddy
+     * @returns a Promise result on the write operation
+     */
+    public async writeToHistory(
+        input: string,
+        buddyKey: BuddyKey
+    ) {
+        if (!buddyKey || typeof buddyKey.userId == 'undefined') {
+            console.log('Buddy key set incorrectly...');
+            return '';
+        }
+
+        const key = this.generateRedisBuddyKey(buddyKey);
+        const result = await this.history.zadd(key, {
+            score: Date.now(),
+            member: input,
+        });
+
+        return result;
+    }
+
+    /**
+     * A public function to query the latest chat history from Redis
+     * @param buddyKey the Prop containing the name, model, and userId of the AI Buddy
+     * @returns a string containing the chat history
+     */
+    public async readLatestHistory(buddyKey: BuddyKey) : Promise<string>{
+        if (!buddyKey || typeof buddyKey.userId == 'undefined') {
+            console.log('Buddy key set incorrectly...');
+            return '';
+        }
+
+        const key = this.generateRedisBuddyKey(buddyKey)
+        let result = await this.history.zrange(key, 0, Date.now(), {
+            byScore: true
+        });
+
+        // modify vector db result to usable format
+        result = result.slice(-30).reverse();
+        const recentChat = result.reverse().join('\n');
+
+        return recentChat;
+    }
+
+    /**
+     * A public function to seed the chat history to Redis 
+     * @param seedContent 
+     * @param delimiter 
+     * @param buddyKey 
+     */
+    public async seedChatHistory(
+        seedContent: string,
+        delimiter: string = '\n',
+        buddyKey: BuddyKey
+    ) {
+        const key = this.generateRedisBuddyKey(buddyKey);
+     
+        if (await this.history.exists(key)) {
+            console.log('User already has chat history');
+            return;
+        }
+
+        const content = seedContent.split(delimiter);
+        let count = 0;
+
+        for (const line of content) {
+            await this.history.zadd(key, {
+                score: count,
+                member: line
+            })
+            count += 1
+        }
+    }
 }
