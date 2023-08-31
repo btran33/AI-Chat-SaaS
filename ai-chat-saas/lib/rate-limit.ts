503d8ad513f62ce283566ff1e3dea8a9ddeca36a
Ben Tran <btran33@illinois.edu>
2023-08-31 15:58:19 -0500

    Create rate limiter to Redis chat history

---
+++ ai-chat-saas/lib/rate-limit.ts
@@ -0,0 +1,19 @@
+import { Ratelimit } from '@upstash/ratelimit'
+import { Redis } from '@upstash/redis'
+
+/**
+ * A rate limiter to Redis chat history
+ * @param identifier the identifier, in string
+ * @returns 
+ */
+export async function rateLimit(identifier: string){
+    // 1 request per second
+    const rateLimit = new Ratelimit({
+        redis: Redis.fromEnv(),
+        limiter: Ratelimit.slidingWindow(10, '10 s'),
+        analytics: true,
+        prefix: '@upstas/ratelimit'
+    });
+
+    return await rateLimit.limit(identifier);
+}
