3a2ada843f892ee3cc50a0497d52d3510e1297ed
Ben Tran <btran33@illinois.edu>
2023-09-01 19:13:31 -0500

    Add extra delete to Cloudinary after the Buddy is terminated

--- ai-chat-saas/app/api/buddy/[buddyId]/route.ts
+++ ai-chat-saas/app/api/buddy/[buddyId]/route.ts
@@ -1,6 +1,8 @@
 import prismaDB from "@/lib/prismadb"
 import { auth, currentUser } from "@clerk/nextjs"
 import { NextResponse } from "next/server"
+import axios from "axios"
+import crypto from "crypto"
 
 export async function PATCH(
     req: Request,
@@ -67,9 +69,59 @@ export async function DELETE(
             }
         })
 
+        // cascade the delete to Cloudinary delete the Buddy's image
+        const cloudinary_src = buddy.src
+        await handleCloudinaryDelete(cloudinary_src)
+
         return NextResponse.json(buddy)
     } catch (error) {
         console.log("[BUDDY_DELETE]", error)
         return new NextResponse("Internal error", { status: 500 })
+    }
+}
+
+export const handleCloudinaryDelete = async (CloudinaryUrl: string) => {
+    // --------------Helper functions--------------
+    const getPublicIdFromUrl = (url: string) => {
+        const regex = /\/v\d+\/([^/]+)\.\w{3,4}$/;
+        const match = url.match(regex);
+        return match ? match[1] : null;
+    };
+
+    const generateSHA1 = (data: any) => {
+        const hash = crypto.createHash('sha1');
+        hash.update(data);
+        return hash.digest('hex');
+    };
+
+    const generateSignature = (publicId: string, timestamp: number) => {
+        if (!publicId) {
+            console.log('[SIGNATURE_ERROR] no public ID was given')
+            return 
+        }
+        const apiSecret = process.env.CLOUDINARY_API_SECRET;
+        return `public_id=${publicId}&timestamp=${timestamp}${apiSecret}`;
+    }
+    // --------------------------------------------
+
+    const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME
+    const apiKey = process.env.CLOUDINARY_API_KEY
+    const timestamp = new Date().getTime();
+    const publicId = getPublicIdFromUrl(CloudinaryUrl)
+
+    const signature = generateSHA1(generateSignature(publicId!, timestamp));
+    const deleteUrl = `https://api.cloudinary.com/v1_1/${cloudName}/image/destroy`;
+
+    try {
+        const response = await axios.post(deleteUrl, {
+            public_id: publicId,
+            signature: signature,
+            api_key: apiKey,
+            timestamp: timestamp
+        })
+
+        // console.log('[CLOUDINARY] ',response)
+    } catch (error) {
+        console.log('[BUDDY_DELETE]',error);
     }
 }
