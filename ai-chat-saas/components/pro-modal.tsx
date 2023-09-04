036e7c4a88ac4eb53d3a119d7e5ad183c86fb0b5
Ben Tran <btran33@illinois.edu>
2023-09-04 14:23:45 -0500

    Add subscription functionality to button and redirect to stripe API

--- ai-chat-saas/components/pro-modal.tsx
+++ ai-chat-saas/components/pro-modal.tsx
@@ -10,9 +10,31 @@ import {
 } from "@/components/ui/dialog"
 import { Separator } from "@/components/ui/separator"
 import { Button } from "@/components/ui/button"
+import { useToast } from "@/components/ui/use-toast"
+import { useState } from "react"
+import axios from "axios"
 
 export const ProModal = () => {
     const proModal = useProModal()
+    const { toast } = useToast()
+    const [loading, setLoading] = useState(false)
+
+    const onSubscribe = async () => {
+        try{
+            setLoading(true)
+
+            const response = await axios.get('/api/stripe')
+
+            window.location.href = response.data.url
+        } catch (error) {
+            toast({
+                variant: 'destructive',
+                description: 'Something went wrong...'
+            })
+        } finally {
+            setLoading(false)
+        }
+    }
 
     return (
         <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
@@ -40,7 +62,7 @@ export const ProModal = () => {
                             .99 / month
                         </span>
                     </p>
-                    <Button variant='premium'>
+                    <Button variant='premium' onClick={onSubscribe} disabled={loading}>
                         Subscribe
                     </Button>
                 </div>
