5802267dcc68c3b65cf975af32c2998791cd0676
Ben Tran <btran33@illinois.edu>
2023-09-04 13:35:39 -0500

    Add Stripe webhook route and handle subscription events

---
+++ ai-chat-saas/app/api/webhook/route.ts
@@ -0,0 +1,73 @@
+import Stripe from "stripe";
+import { headers } from 'next/headers'
+import { NextResponse } from "next/server";
+
+import prismaDB from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+
+export async function POST(req: Request) {
+    const body = await req.text()
+    const signature = headers().get('Stripe-Signature') as string;
+
+    let event: Stripe.Event
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET!
+        )
+    } catch (error: any) {
+        console.log('[WEBHOOK_POST]', error)
+        return new NextResponse(
+            `Webhook Error: ${error.message}`,
+            { status : 400 }
+        )
+    }
+
+    const session = event.data.object as Stripe.Checkout.Session;
+
+    // first time subscription
+    if (event.type === 'checkout.session.completed') {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        )
+
+        if (!session?.metadata?.userId) {
+            return new NextResponse('User ID is required', { status: 400})
+        }
+
+        await prismaDB.userSubscription.create({
+            data: {
+                userId: session?.metadata?.userId,
+                stripeSubscriptionId: subscription.id,
+                stripeCustomerId: subscription.customer as string,
+                stripePriceId: subscription.items.data[0].price.id,
+                stripeCurrentPeriodEnd: new Date(
+                    subscription.current_period_end * 1000
+                )
+            }
+        })
+    }
+
+    // recurring subscription
+    if (event.type === 'invoice.payment_succeeded') {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription as string
+        )
+
+        await prismaDB.userSubscription.update({
+            where: {
+                stripeSubscriptionId: subscription.id
+            },
+            data: {
+                stripePriceId: subscription.items.data[0].price.id,
+                stripeCurrentPeriodEnd: new Date(
+                    subscription.current_period_end * 1000
+                )
+            }
+        })
+    }
+
+    return new NextResponse(null, { status: 200 })
+}
