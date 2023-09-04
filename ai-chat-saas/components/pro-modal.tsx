8d5e46f39d1a885068326a35dd9513554ad384ed
Ben Tran <btran33@illinois.edu>
2023-09-04 14:07:15 -0500

    Style the proModal to include subscription information

--- ai-chat-saas/components/pro-modal.tsx
+++ ai-chat-saas/components/pro-modal.tsx
@@ -4,9 +4,12 @@ import { useProModal } from "@/hooks/use-pro-modal"
 import { 
     Dialog, 
     DialogContent, 
-    DialogHeader 
+    DialogDescription, 
+    DialogHeader,
+    DialogTitle
 } from "@/components/ui/dialog"
-import { DialogTitle } from "@radix-ui/react-dialog"
+import { Separator } from "@/components/ui/separator"
+import { Button } from "@/components/ui/button"
 
 export const ProModal = () => {
     const proModal = useProModal()
@@ -18,7 +21,29 @@ export const ProModal = () => {
                     <DialogTitle className="text-center">
                         Upgrade to Pro
                     </DialogTitle>
+
+                    <DialogDescription className="text-center space-y-2">
+                        Create 
+                        <span className="text-sky-500 font-medium mx-1">
+                            Custom AI
+                        </span> 
+                        Buddy!
+                    </DialogDescription>
                 </DialogHeader>
+
+                <Separator/>
+
+                <div className="flex justify-between">
+                    <p className="text-2xl font-medium">
+                        $9
+                        <span className="text-sm font-normal">
+                            .99 / month
+                        </span>
+                    </p>
+                    <Button variant='premium'>
+                        Subscribe
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
     )
